2814e8c8d6d64f3cc4c1a88f1e85e171c5689c1e
ابو يحيى عبدالله <abdalla-yahia>
2025-09-21 13:51:50 +0300

    Edit DropDown Of Locale

--- src/Components/Header/Header_Container.tsx
+++ src/Components/Header/Header_Container.tsx
@@ -6,10 +6,12 @@ import { useEffect, useRef, useState } from "react";
 import Login_User from "./Login_User";
 import { CiMenuBurger } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
-
 import styles from './style.module.css';
+import { RootState, useAppDispatch, useAppSelector } from "@/Libs/Store/Store";
+import { getAllLanguagies } from "@/Features/Actions/LanguageActions";
 
 export default function Header_Container({currentLocale}: {currentLocale: string}) {
+  const {AllLanguages} = useAppSelector((state:RootState)=>state.language)
   const [language,setLanguage] = useState(currentLocale)
   const [flag,setFlag] = useState('')
   const [toggle,setToggle] = useState(false)
@@ -18,6 +20,12 @@ export default function Header_Container({currentLocale}: {currentLocale: string
   const dropDownRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const pathname = usePathname();
+
+  const dispatch = useAppDispatch()
+  //Get All Locale Existes
+  useEffect(()=>{
+    dispatch(getAllLanguagies())
+  },[dispatch])
   //Close DropDown If User Click On Body
   useEffect(()=>{
     function ClickOutSideDropDownHandler(e: MouseEvent) {
@@ -59,46 +67,38 @@ export default function Header_Container({currentLocale}: {currentLocale: string
     }
   },[currentLocale])
   return (
-    <header className="parent z-50 top-0 left-0 text-primary bg-secondary relative">
-      <div className="container bg-inherit">
+    <header className=" w-full flex justify-center items-center z-50 top-0 left-0 text-primary bg-secondary relative">
+      <div className="container w-full bg-inherit">
         {/*Logo && Locale*/}
-        <div className="flex z-50 gap-4 justify-center items-center">
-          {/*Logo*/}
+        <div className="flex z-50 gap-4 bg-inherit justify-center items-center">
+            {/*Logo*/}
             <Link href='/'>
-              <Image priority src={'/Images/Logo.png'} alt="logo" width={220} height={100}/>
+              <Image priority src={'/Images/Logo.png'} className=" drop-shadow-xl" alt="logo" width={220} height={100}/>
             </Link>
-            <div ref={dropDownRef} onClick={()=>setToggle(!toggle)} className="relative cursor-pointer flex flex-col justify-center items-center gap-1 w-full">
+            {/*List Of Locale*/}
+            <div ref={dropDownRef} onClick={()=>setToggle(!toggle)} className="relative bg-inherit cursor-pointer flex flex-col justify-center items-center gap-1 w-full">
               <p className="border uppercase p-1 flex justify-between items-center gap-1">{language === 'ar'?'ع':language}
               <Image  src={flag} alt={`flag-${language}`} width={20} height={20}/>
               </p>
-              {toggle && <div  className="flex   px-2 flex-col justify-between items-center gap-2 absolute top-[120%] left-[25%]">
-                <span onClick={()=>{setLanguage('ar');setToggle(false)}} className="hover:bg-secondary px-1 cursor-pointer flex justify-between items-center gap-1 w-full">ع
-                <Image  src={'https://static.vecteezy.com/system/resources/previews/001/952/861/non_2x/egypt-flag-isolate-banner-print-illustration-eps-vector.jpg'} alt={`flag-${language}`} width={20} height={20}/>
-                </span>
-                <span onClick={()=>{setLanguage('en');setToggle(false)}} className="hover:bg-secondary px-1 cursor-pointer flex justify-between items-center gap-1 w-full">En
-                <Image src={'https://static.vecteezy.com/system/resources/previews/002/417/819/non_2x/illustration-of-the-united-kingdom-flag-free-vector.jpg'} alt={`flag-${language}`} width={20} height={20} />
-                </span>
-                <span onClick={()=>{setLanguage('fr');setToggle(false)}} className="hover:bg-secondary px-1 cursor-pointer flex justify-between items-center gap-1 w-full">Fr
-                <Image src={'https://static.vecteezy.com/system/resources/previews/004/313/578/non_2x/france-country-flag-free-vector.jpg'} alt={`flag-${language}`} width={20} height={20} />
-                </span>
-                <span onClick={()=>{setLanguage('sp');setToggle(false)}} className="hover:bg-secondary px-1 cursor-pointer flex justify-between items-center gap-1 w-full">Sp
-                <Image src={'https://static.vecteezy.com/system/resources/previews/009/767/106/non_2x/spain-flag-flag-of-spain-illustration-free-vector.jpg'} alt={`flag-${language}`} width={20} height={20} />
-                </span>
-                <span onClick={()=>{setLanguage('it');setToggle(false)}} className="hover:bg-secondary px-1 cursor-pointer flex justify-between items-center gap-1 w-full">It
-                <Image src={'https://static.vecteezy.com/system/resources/previews/002/417/790/non_2x/vectorial-illustration-of-the-italian-flag-free-vector.jpg'} alt={`flag-${language}`} width={20} height={20} />
-                </span>
-                <span onClick={()=>{setLanguage('zh');setToggle(false)}} className="hover:bg-secondary px-1 cursor-pointer flex justify-between items-center gap-1 w-full">Zh
-                <Image src={'https://static.vecteezy.com/system/resources/previews/002/133/523/non_2x/chinese-flag-official-chinese-flag-with-original-color-and-size-proportion-free-vector.jpg'} alt={`flag-${language}`} width={20} height={20} />
-                </span>
+              {toggle && <div  className="flex w-full  flex-col justify-between items-center gap-2 absolute top-[200%] left-[50%] -translate-x-[50%] bg-inherit">
+                {
+                  AllLanguages?.Language?.map((lang)=>
+                  <span key={lang?.id} onClick={()=>{setLanguage(lang?.code as string);setToggle(false)}} className="hover:bg-primary hover:text-secondary px-3 cursor-pointer uppercase flex justify-between items-center gap-1 w-full">
+                    {lang?.name}
+                  <Image  src={lang?.flage} alt={`flag-${lang?.code}`} width={20} height={20}/>
+                  </span>
+                  
+                  )
+                }
               </div>}
             </div>
         </div>
         {/*Login && NavList*/}
-        <div className="flex-1 flex bg-inherit gap-4 justify-center items-center ">
+        <div className="w-full flex bg-inherit gap-4 justify-between items-center ">
           {/*Nav List*/}
           <nav style={{
             clipPath:toggleMenuLink && 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'||''
-          }} className={`${styles.navLinkswrapper}`}>
+          }} className={`${styles.navLinkswrapper} w-full`}>
             <ul className="w-full flex  flex-col md:flex-row justify-center items-center gap-2 px-2 ">
               {/* <li className="hover:bg-secondary hover:text-white duration-100 p-2 rounded-full cursor-pointer font-[600]"><Link href="/order" >{t('order')}</Link></li> */}
               <li className="hover:bg-secondary hover:text-white duration-100 p-2 rounded-full cursor-pointer font-[600]"><Link onClick={()=>setToggleMenuLink(false)} href="/" >{t('home')}</Link></li>
